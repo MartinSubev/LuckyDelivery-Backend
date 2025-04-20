5c654efc817f28e416e4c4e622adc5819c89ddfd
MartinSubev <martinsubev23@gmail.com>
2025-04-20 23:55:18 +0300

    +

--- src/main/java/LuckyDelivery/Repository/UserRepository.java
+++ src/main/java/LuckyDelivery/Repository/UserRepository.java
@@ -1,11 +1,14 @@
 package LuckyDelivery.Repository;
 
 import LuckyDelivery.Model.User;
+import LuckyDelivery.Model.User.UserType; // Import the enum
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
+    Optional<User> findByIdAndType(Long id, UserType type); // Changed parameter type
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
